7a7d973c3f4bb4e3ad00369e1bfe33c95bc3cba5
atomwolve <35859306+atomwolve@users.noreply.github.com>
2024-05-24 01:40:28 -0400

    Forgot to make intermediate pushes oops
    - Created simple win/lose screen
    - Added/tested functionality (trigger win/lose through singleton)
    - Added GenericLoadScene to reset (or move to a different scene if we want)
    - Added UIToolkit
    - Created a physical grid script
    - Created physical grid script editor
    - Added mine randomization and clearing

---
+++ Assets/Scripts/Game Logic Scripts/PhysicalGrid.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Map<TKey, TValue>
+{
+	[SerializeField]
+	private List<TKey> keysList = new();
+	public List<TKey> KeysList
+	{
+		get { return keysList; }
+		set { keysList = value; }
+	}
+
+	[SerializeField]
+	private List<TValue> valuesList = new();
+	public List<TValue> ValuesList
+	{
+		get { return valuesList; }
+		set { valuesList = value; }
+	}
+
+	private Dictionary<TKey, TValue> dictionaryData = new();
+	public Dictionary<TKey, TValue> DictionaryData
+	{
+		get { return dictionaryData; }
+		set { dictionaryData = value; }
+	}
+
+	public void UpdateDictionary()
+    {
+		DictionaryData.Clear();
+		for(int i = 0; i < keysList.Count; i++)
+        {
+			DictionaryData.TryAdd(KeysList[i], ValuesList[i]);
+        } 
+    }
+}
+
+public class PhysicalGrid : MonoBehaviour
+{
+    [SerializeField] private Vector2Int dims;
+	[Range(0f, 1f)]
+	[SerializeField] private float minePercentChance;
+	 
+    [Serializable] public class GridMap : Map<Vector2Int, bool> { }
+	//[HideInInspector]
+	public GridMap mineGrid = new();
+
+	private void Start()
+    {
+        foreach (var pair in mineGrid.DictionaryData)
+        {
+            if (pair.Value) print($"MINE AT: ({pair.Key.x}, {pair.Key.y})");
+        }
+    }
+
+	public void RandomizeMines()
+    {
+		foreach(var pair in mineGrid.DictionaryData)
+        {
+			mineGrid.ValuesList[mineGrid.KeysList.IndexOf(pair.Key)] = UnityEngine.Random.Range(0f, 0.99f) < minePercentChance;
+		}
+		mineGrid.UpdateDictionary();
+    }
+	public void ClearMines()
+	{
+		foreach (var pair in mineGrid.DictionaryData)
+		{
+			mineGrid.ValuesList[mineGrid.KeysList.IndexOf(pair.Key)] = false;
+		}
+		mineGrid.UpdateDictionary();
+	}
+}
